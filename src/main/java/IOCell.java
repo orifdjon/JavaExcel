b66e472d472f849054d557e3bfa23504a0556c19
Arif <orif95@mail.ru>
2018-10-28 15:27:33 +0300

    IOCell

---
+++ src/main/java/IOCell.java
@@ -0,0 +1,64 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+
+/**
+ * Created by Arif on Окт., 2018
+ */
+public class IOCell {
+
+    private File filePath;
+
+    private IOCell() {
+    }
+
+    public IOCell(String filePath) {
+        this.filePath = new File(filePath);
+
+    }
+
+    /**
+     * Method return cell. if filePath is specified in constructor is not exist, throw exception
+     *
+     * @param sheet  with 0
+     * @param row    with 0
+     * @param column with 0
+     * @return cell
+     */
+    public Cell getCell(int sheet, int row, int column) {
+        Workbook workbook = null;
+        try (FileInputStream file = new FileInputStream(filePath)) {
+            workbook = new XSSFWorkbook(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("file is not exists");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return workbook.getSheetAt(sheet).getRow(row).getCell(column);
+    }
+
+
+    public void setCell(int row, int column, double val) {
+
+        Workbook workbook = null;
+         try (FileInputStream file = new FileInputStream(filePath)) {
+             workbook = new XSSFWorkbook(file);
+             Sheet sheet = workbook.getSheetAt(0);
+             sheet.getRow(row).getCell(column).setCellValue(val);
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+        try (OutputStream fileOut = new FileOutputStream(filePath)) {
+            workbook.write(fileOut);
+        } catch (FileNotFoundException e) {
+            System.out.println("file is not exist AAAA");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import org.apache.poi.ss.usermodel.Cell;
+
+/**
+ * Created by Arif on Окт., 2018
+ */
+public class Main {
+    //var20 have to exist, if var20.xlsx don't exist then throw IOException
+    //To correct work this program the file have to close in your system!!!
+    private static final String filePath = "var20.xlsx";
+
+    public static void main(String[] args) {
+        IOCell ioCell = new IOCell(filePath);
+
+        Cell x = ioCell.getCell(0, 1, 0);
+        Cell y = ioCell.getCell(0, 1, 1);
+        System.out.println("first number: " + x.toString());
+        System.out.println("second number: " + y.toString());
+        //Write x * y
+        ioCell.setCell(4, 0, x.getNumericCellValue() * y.getNumericCellValue());
+        //Write x + y
+        ioCell.setCell(4, 1, x.getNumericCellValue() + y.getNumericCellValue());
+        System.out.println("Interactions is complete successfully");
+    }
+}
